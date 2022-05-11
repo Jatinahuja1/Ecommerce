8b40893b69cf6e704ba752d7e32cdc27c3e933ad
Rajat Manik <rajat.manik@softuvo.com>
2022-05-11 18:16:47 +0530

    Added Hompage

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.pngmart.com/files/1/Mens-Fashion-PNG-Pic.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPOMISE WITH STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://us.123rf.com/450wm/ddkolos/ddkolos1909/ddkolos190900242/130115773-young-girl-in-a-business-dress-wearing-clothes-pretty-smiling-on-a-white-background-a-slender-female.jpg",
+    title: "WINTER SALE",
+    desc: "DON'T COMPOMISE WITH STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+  {
+    id: 3,
+    img: "https://cdn.vmartretail.com/images1/thumbnails/97187/640/1/143694228-97187495-1640350193.jpg",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPOMISE WITH STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.freegreatpicture.com/files/photo104/51828-blue-jeans-white-vest-girls.jpg",
+    title: "GIRLS WEAR!",
+  },
+  {
+    id: 2,
+    img: "https://cdn.linenclub.com/media/catalog/product/cache/5399faaab5f4f7a53013d830013bdee7/c/a/camsfyd51601410-n5_2.jpg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 3,
+    img: "https://www.pngkey.com/png/detail/366-3661819_religion-leather-jackets-men.png",
+    title: "JACKETS!",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/819qvacQhiL._AC_SX255_.jpg",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71g8x8zst3S._AC_SX255_.jpg",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/81p6n6u4AwL._AC_SX255_.jpg",
+  },
+  {
+    id: 4,
+    img: "https://www.freegreatpicture.com/files/photo104/51828-blue-jeans-white-vest-girls.jpg",
+  },
+  {
+    id: 5,
+    img: "https://cdn.linenclub.com/media/catalog/product/cache/5399faaab5f4f7a53013d830013bdee7/c/a/camsfyd51601410-n5_2.jpg",
+  },
+  {
+    id: 6,
+    img: "https://www.pngkey.com/png/detail/366-3661819_religion-leather-jackets-men.png",
+  },
+];
